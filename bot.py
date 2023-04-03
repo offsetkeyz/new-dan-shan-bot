11394413037c5758f274c659e83de81a8a38e4e8
Colin <colin.mcallister89@yahoo.com>
2023-04-02 23:50:52 -0400

    fixing time to be readable in discord

--- bot.py
+++ bot.py
@@ -119,7 +119,7 @@ async def status_task() -> None:
     Setup the game status task of the bot
     """
     # statuses = ["the skin flute", "with himself", 'pornhub.com', 'with rogaine', 'who can I demote next', 'with mister Avant']
-    statuses = [f"Updated {datetime.datetime.now()}"]
+    statuses = [f'Updated {datetime.datetime.now().strftime("%d %H:%M")}']
     await bot.change_presence(activity=discord.Game(random.choice(statuses)))
 
 
